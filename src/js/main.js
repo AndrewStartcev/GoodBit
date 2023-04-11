a4db4874b42fa17488b7cbb6abed4e84e8fd6568
Андрей <40750040+AndrewStartcev@users.noreply.github.com>
2023-04-11 16:54:32 +0800

    add header

--- src/js/main.js
+++ src/js/main.js
@@ -2,3 +2,33 @@ import { handleHeroResize } from './modules/hero-swiper.js';
 
 window.addEventListener('load', handleHeroResize);
 window.addEventListener('resize', handleHeroResize);
+
+
+const menuItems = document.querySelectorAll('.menu-item');
+
+menuItems.forEach((menuItem) => {
+  const subMenu = menuItem.querySelector('.sub-menu');
+
+  if (subMenu) {
+    menuItem.addEventListener('mouseenter', () => {
+      subMenu.style.display = 'block';
+    });
+
+    menuItem.addEventListener('mouseleave', () => {
+      subMenu.style.display = 'none';
+    });
+
+    menuItem.addEventListener('click', (event) => {
+      event.preventDefault();
+      subMenu.style.display = subMenu.style.display === 'block' ? 'none' : 'block';
+    });
+  }
+});
+
+const burgerButton = document.querySelector('.header__burger');
+const menu = document.querySelector('.menu');
+
+burgerButton.addEventListener('click', function () {
+  burgerButton.classList.toggle('active');
+  menu.classList.toggle('menu--active');
+});
