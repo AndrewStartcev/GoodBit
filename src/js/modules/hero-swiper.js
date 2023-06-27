aeb0f790d472ebf68b24967739eee96b4520f353
Андрей <40750040+AndrewStartcev@users.noreply.github.com>
2023-06-27 21:12:11 +0800

    Add portfilo page

--- src/js/modules/hero-swiper.js
+++ src/js/modules/hero-swiper.js
@@ -1,62 +1,74 @@
 const heroFeatures = document.querySelector('.hero-features');
-const heroFeaturesWrapper = document.querySelector('.hero-features__wrapper');
-const heroFeatureSlide = heroFeatures.querySelectorAll('.hero-features__slide');
+let heroFeaturesWrapper = null
+let heroFeatureSlide = null
 let heroSwiper = undefined;
 
-function createHeroSwiper() {
-  const swiperPagination = document.createElement('div')
-
-  swiperPagination.classList.add('swiper-pagination');
-  swiperPagination.classList.add('features-pagination');
-
-  heroFeaturesWrapper.classList.add('swiper-wrapper');
-
-  heroFeatureSlide.forEach((card) => {
-    card.classList.add('swiper-slide');
-  });
-
-  heroFeatures.appendChild(swiperPagination);
-
-  heroSwiper = new Swiper(heroFeatures, {
-    spaceBetween: 15,
-
-    pagination: {
-      el: swiperPagination,
-      clickable: true,
-      dynamicBullets: true
-    },
-  });
+if (heroFeatures != undefined) {
+  heroFeaturesWrapper = document.querySelector('.hero-features__wrapper');
+  heroFeatureSlide = heroFeatures.querySelectorAll('.hero-features__slide');
 }
 
+
 function destroyHeroSwiper() {
+  if (heroFeatureSlide.length > 0) {
+    heroFeatures.classList.remove('swiper-wrapper');
 
-  heroFeatures.classList.remove('swiper-wrapper');
-
-  heroFeatureSlide.forEach((card) => {
-    card.classList.remove('swiper-slide');
-  });
-
-  if (heroSwiper) {
-    let pagination = document.querySelectorAll('.features-pagination');
-    let note = document.querySelectorAll('.swiper-notification');
-
-    pagination.forEach((el) => {
-      el.remove();
+    heroFeatureSlide.forEach((card) => {
+      card.classList.remove('swiper-slide');
     });
-    note.forEach((el) => {
-      el.remove();
+
+    if (heroSwiper) {
+      let pagination = document.querySelectorAll('.features-pagination');
+      let note = document.querySelectorAll('.swiper-notification');
+
+      pagination.forEach((el) => {
+        el.remove();
+      });
+      note.forEach((el) => {
+        el.remove();
+      });
+      heroSwiper.destroy();
+    }
+  }
+}
+function createHeroSwiper() {
+  if (heroFeatureSlide.length > 0) {
+    const swiperPagination = document.createElement('div')
+
+    swiperPagination.classList.add('swiper-pagination');
+    swiperPagination.classList.add('features-pagination');
+
+    heroFeaturesWrapper.classList.add('swiper-wrapper');
+
+    heroFeatureSlide.forEach((card) => {
+      card.classList.add('swiper-slide');
     });
-    heroSwiper.destroy();
+
+    heroFeatures.appendChild(swiperPagination);
+
+    heroSwiper = new Swiper(heroFeatures, {
+      spaceBetween: 15,
+
+      pagination: {
+        el: swiperPagination,
+        clickable: true,
+        dynamicBullets: true
+      },
+    });
   }
 }
 
 export function handleHeroResize() {
-  const screenWidth = window.innerWidth;
-  const isMobile = screenWidth < 768;
+  if (heroFeatureSlide.length > 0) {
+    const screenWidth = window.innerWidth;
+    const isMobile = screenWidth < 768;
 
-  if (isMobile) {
-    createHeroSwiper();
-  } else {
-    destroyHeroSwiper();
+    if (isMobile) {
+      createHeroSwiper();
+    } else {
+      destroyHeroSwiper();
+    }
+
   }
+
 }
